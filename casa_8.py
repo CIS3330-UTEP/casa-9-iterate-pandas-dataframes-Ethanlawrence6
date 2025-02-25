37a58a1abce70ffb6beb0e71c894739663d3a730
Ethanlawrence6 <erlawrence@miners.utep.edu>
2025-02-25 12:58:41 -0700

    attempt 1

--- casa_8.py
+++ casa_8.py
@@ -1,1 +1,17 @@
+import pandas as pd
 
+df = pd.read_csv("big-mac-full-index.csv")
+
+print("\n--- Iterating using iterrows() ---")
+
+for index, row in df.iterrows():
+    print(f"Country: {row['name']}, Year: {row['year']}, Price: {row['local_price']}")
+    if index == 4:
+        break
+
+print("\n--- Iterating using apply() ---")
+
+def print_row(row):
+    print(f"Country: {row['name']}, Year: {row['year']}, Price: {row['local_price']}")
+
+df.head(5).apply(print_row, axis=1)
